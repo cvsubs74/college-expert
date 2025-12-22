49fd2fffd3c0fded3a6aa9111e9f74972405baa7
User <user@example.com>
2025-12-22 08:27:42 -0800

    CLEANUP: Remove temporary BUILD_VERSION cache busting code

--- frontend/src/main.jsx
+++ frontend/src/main.jsx
@@ -3,10 +3,6 @@ import ReactDOM from 'react-dom/client'
 import App from './App.jsx'
 import './index.css'
 
-// Build version to force cache bust - update this when deploying
-const BUILD_VERSION = '2024.12.22.v2';
-console.log('[App] Build version:', BUILD_VERSION);
-
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
     <App />
